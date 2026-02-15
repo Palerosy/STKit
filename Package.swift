c9f36639f0640f3ef668591c1c3eef54a997b24b
Sametcan <sametcan@Sametcans-MacBook-Air.local>
2026-02-15 21:57:15 +0400

    Switch to library-format static XCFrameworks for proper SPM linking

--- Package.swift
+++ Package.swift
@@ -14,27 +14,27 @@ let package = Package(
         .binaryTarget(
             name: "STKit",
             url: "https://github.com/Palerosy/STKit/releases/download/0.1.0/STKit.xcframework.zip",
-            checksum: "20a9accb7b696e7830f1d0c71c3e09c5712662e8125fd78bbdea8c467894a040"
+            checksum: "a4bc515788f8339c00ab4e55fe9c45471d3e41fd60533c5acc70b161d46add75"
         ),
         .binaryTarget(
             name: "STDOCX",
             url: "https://github.com/Palerosy/STKit/releases/download/0.1.0/STDOCX.xcframework.zip",
-            checksum: "980a5d4c7c271d2fd0940a3354d205914257d26b61bb1d49436ccaa4932264f1"
+            checksum: "a9403a898db31903f8323a474e80dee5c45090e438bbebebe3cfffbbde91ab8d"
         ),
         .binaryTarget(
             name: "STExcel",
             url: "https://github.com/Palerosy/STKit/releases/download/0.1.0/STExcel.xcframework.zip",
-            checksum: "a63e858a539aec80e6800d94a9d8c4667117bc11eb49eb6f697c10e849ed3fda"
+            checksum: "ab4861235d5e0b453c225213211c0a4b2d2f82ce8b24923588281a5df6a22234"
         ),
         .binaryTarget(
             name: "STTXT",
             url: "https://github.com/Palerosy/STKit/releases/download/0.1.0/STTXT.xcframework.zip",
-            checksum: "bc53fb5562ee30301de3159c55a5078c4469a93fb4f292da9e07f9cc32d6adc7"
+            checksum: "3cc66c7adc56c7df0dea8c6fbe17787b188d7f21f20261edaac2aec9ee81ab53"
         ),
         .binaryTarget(
             name: "_ZIPFoundation",
             url: "https://github.com/Palerosy/STKit/releases/download/0.1.0/_ZIPFoundation.xcframework.zip",
-            checksum: "20745d04fcc5b47f08091e43fcadb5c671b24a677e49aa0b7b725a3c0a8ea95b"
+            checksum: "1cda4a5a577e0eb5e9951fcf131aaee338f3e06e8ee3f443e0f79404e3636ea7"
         ),
     ]
 )
